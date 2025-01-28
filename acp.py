21887f33292b672c89dad385b401c0695ce69800
Nur-A-Sabah <sajedurkabir@gmail.com>
2025-01-28 08:29:35 -0800

    acp

---
+++ acp.py
@@ -0,0 +1,10 @@
+age = int(input("How old are you ? : "))
+
+if age > 9:
+    
+    if age >= 10 and age <= 20:
+        print("You are allowed in the class")
+    else:
+        print("You are too old for the class")
+else:
+    print("You are too young for the class")
